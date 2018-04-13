5cdfe2dbbdf36b549de987e82e658d35b0822dd0
X-style <wangchongtech@msn.com>
2018-04-13 14:02:06 +0800

    tool file update

---
+++ scripts/common.js
@@ -0,0 +1,3 @@
+//公共js
+//禁用键盘/右键
+//document.oncontextmenu=function(){return false;};document.onkeydown=function(event){if ((event.keyCode==112)||(event.keyCode==113)||(event.keyCode==114)||(event.keyCode==115)||(event.keyCode==116)||(event.keyCode==118)||(event.keyCode==119)||(event.keyCode==120)||(event.keyCode==121)||(event.keyCode==122)||(event.keyCode==123)||(event.ctrlKey&&event.keyCode==84)||(event.ctrlKey&&event.keyCode==78)||(event.shiftKey&&event.keyCode==121)){/*alert("F1-F12/Ctrl+T/Ctrl+N/Shift+F10");*/event.keyCode=0;return false;}};
